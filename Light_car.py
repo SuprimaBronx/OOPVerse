2ddf88e94510f355a3d8ef8ba9f61b27a3728a34
SuprimaBronx <k7iao@yandex.ru>
2021-05-13 12:41:10 +0700

    resolving class conflicts

--- Light_car.py
+++ Light_car.py
@@ -10,7 +10,13 @@ class Light_car(Cars):
     def In(self, line):
         self.max_speed = line[1]
         self.engine_power = line[2]
+        self.fuel_consumption = line[3]
+        self.global_weight = 4 * 75
+        self.ratio_calc()
 
     def display(self, file, i):
         file.write(str(i) + '.Легковая машина' + "\n")
         file.write('Максимальная скорость: ' + str(self.max_speed) + "\n")
+
+    def display_filtr(self, file_name, i):
+        return None
