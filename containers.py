6f7241664867d58b5d1226d8db01744ddd54c744
SuprimaBronx <k7iao@yandex.ru>
2021-05-20 15:15:50 +0700

    add defence

--- containers.py
+++ containers.py
@@ -16,18 +16,36 @@ class Container:
             self.cars.append(cd)
 
 
-    def out(self, file):
-        file.write('Контейнер содержит ' + str(len(self.cars))+' элементов \n')
-        for i in range(len(self.cars)):
-            self.cars[i].display(file, i)
-            file.write('мощность двигателя: ' + str(self.cars[i].engine_power) + "\n")
-            file.write('расход топлива: ' + str(self.cars[i].fuel_consumption) + "\n")
-            file.write('Отношение веса груза к мощности двигателя : ' + str(self.cars[i].ratio) + "\n")
+    def out(self, file,x):
+        if x == 0:
+            file.write('в вводимых данных содержится ошибка, неизвестный символ')
+        elif x == 1:
+            file.write('в вводимых данных содержится ошибка, выбран несуществующий класс автомобилей')
+        elif x ==2:
+            file.write('в вводимых данных содержится ошибка, параметры автомобилей не могут быть отирцательными')
+        elif x == 4:
+            file.write('неверно введённое имя входного файла')
+        else:
+            file.write('Контейнер содержит ' + str(len(self.cars)) + ' элементов \n')
+            for i in range(len(self.cars)):
+                self.cars[i].display(file, i)
+                file.write('мощность двигателя: ' + str(self.cars[i].engine_power) + "\n")
+                file.write('расход топлива: ' + str(self.cars[i].fuel_consumption) + "\n")
+                file.write('Отношение веса груза к мощности двигателя : ' + str(self.cars[i].ratio) + "\n")
 
-    def out_filtr(self, file_name):
-        file_name.write('Контейнер содержит ' + str(len(self.cars)) + ' элементов \n')
-        for i in range(len(self.cars)):
-            self.cars[i].display_filtr(file_name, i)
+    def out_filtr(self, file_name,x):
+        if x == 0:
+            file_name.write('в вводимых данных содержится ошибка, неизвестный символ')
+        elif x == 1:
+            file_name.write('в вводимых данных содержится ошибка, выбран несуществующий класс автомобилей')
+        elif x == 2:
+            file_name.write('в вводимых данных содержится ошибка, параметры автомобилей не могут быть отирцательными')
+        elif x == 4:
+            file_name.write('неверно введённое имя входного файла')
+        else:
+            file_name.write('Контейнер содержит ' + str(len(self.cars)) + ' элементов \n')
+            for i in range(len(self.cars)):
+                self.cars[i].display_filtr(file_name, i)
 
     def sort(self):
         for i in range(len(self.cars)-1):
@@ -38,3 +56,18 @@ class Container:
     def clear(self):
         self.cars.clear()
 
+    def check(self,file):
+        for line in file:
+            temp = (line + '.')[:-1]
+            for i in range(len(temp)):
+                if temp[i] not in '|\n0123456789':
+                    return 0
+            temp = temp.rstrip('\n')
+            temp = temp.split('|')
+            if temp[0] not in '012':
+                return 1
+            for i in range(len(temp)):
+                temp[i] = int(temp[i])
+            if temp[1] <= 0 or temp[2] <= 0 or temp[3] <= 0:
+                return 2
+        return 'x'
--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 from containers import Container
 
 def main():
+    x = 0
     container = Container()
     input_name = str(input())
     output_name = str(input())
     output_filtr_name = str(input())
-    file = open(input_name)
-    container.in_data(file)
+    try:
+        file = open(input_name)
+    except:
+        x = 4
+    if x!=4:
+        x = container.check(file)
+        file.close()
+    if x == 'x':
+        file = open(input_name)
+        container.in_data(file)
+        file.close()
+        container.sort()
+        file = open(output_name, "w")
+        file1 = open(output_filtr_name, "w")
+        container.out(file,x)
+        container.out_filtr(file1,x)
+        container.clear()
+        container.out(file,x)
+        container.out_filtr(file1,x)
+    if x != 'x':
+        file = open(output_name, "w")
+        file1 = open(output_filtr_name, "w")
+        container.out(file, x)
+        container.out_filtr(file1, x)
     file.close()
-    container.sort()
-    file = open(output_name, "w")
-    file1 = open(output_filtr_name, "w")
-    container.out(file)
-    container.out_filtr(file1)
-    container.clear()
-    container.out(file)
-    container.out_filtr(file1)
-    file.close()
-
+    file1.close()
 if __name__ == '__main__':
     main()
